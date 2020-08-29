1449e4971ab12de768aa73b84086489941273508
nsedan <naim.alejandro.sedan@gmail.com>
2020-08-29 07:24:48 +0000

    base and tasks templates created

--- app.py
+++ app.py
@@ -1,13 +1,30 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+from os import path
+if path.exists("env.py"):
+    import env
 
 app = Flask(__name__)
 
+# MONGODB_URI = os.environ.get('MONGO_URI')
+# DBS_NAME = "milestoneTestDB"
+# COLLECTION_NAME = "moviesTestDB"
+
+app.config["MONGO_DBNAME"] = "task_manager"
+app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
+
+mongo = PyMongo(app)
+
+
 @app.route('/')
-def hello():
-    return "Hello World!"
+@app.route('/get_tasks')
+def get_tasks():
+    return render_template("tasks.html", tasks=mongo.db.tasks.find())
+
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
-    port=int(os.environ.get('PORT')),
-    debug=True)
+            port=int(os.environ.get('PORT')),
+            debug=True)
